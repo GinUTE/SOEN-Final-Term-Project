2f72343b1789a547044c538f1bb848dd86dded14
Deskind <91470404+GinUTE@users.noreply.github.com>
2022-12-11 08:22:21 +0700

    Create MovieDAO in DAL

---
+++ movie-ticket-booking-system/DAL/MovieDAO.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+using movie_ticket_booking_system.DL;
+
+namespace movie_ticket_booking_system.DAL
+{
+    internal class MovieDAO
+    {
+        private readonly DbConnection _dbConnection;
+
+        public MovieDAO()
+        {
+            _dbConnection = new DbConnection();
+        }
+
+        public DataTable GetNowShowing()
+        {
+            return _dbConnection.ExecuteLoadQuery("usp_GetNowShowing", cmdType: CommandType.StoredProcedure);
+        }
+
+        public DataTable GetComingSoon()
+        {
+            return _dbConnection.ExecuteLoadQuery("usp_GetComingSoon", cmdType: CommandType.StoredProcedure);
+        }
+    }
+}
