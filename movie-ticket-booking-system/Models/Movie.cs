800822f509a506a9810aa858341feee6d9f71d7c
Deskind <91470404+GinUTE@users.noreply.github.com>
2022-12-11 08:21:52 +0700

    Create Movie Model

---
+++ movie-ticket-booking-system/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿namespace movie_ticket_booking_system.Models
+{
+    internal class Movie
+    {
+        public string MovieId { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string ReleaseDate { get; set; }
+        public string Runtime { get; set; }
+        public string Description { get; set; }
+        public string Genre { get; set; }
+        public byte[] Thumbnail { get; set; }
+    }
+}
